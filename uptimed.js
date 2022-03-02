ca240100b6171b504882ae1c7b7aacca2ae8a941
J-45 <64985480+J-45@users.noreply.github.com>
2022-03-02 12:37:23 +0100

    added notification

--- uptimed.js
+++ uptimed.js
@@ -1,34 +1,106 @@
 const puppeteer = require('puppeteer');
+const https = require('https');
 
 (async () => {
-    while (true) {
+
         console.log("-= Uptimed.js =-\n");
         const PARAMETERS    = process.argv.toString();
-        let URL             = ""
-        let TEXT            = ""
-        let DELAY           = ""
+        let URL             = "";
+        let TEXT            = "";
+        let DELAY           = "";
+        let TIMEOUT         = "";
+        let upAndRunning    = null;
+        let lastNotification= 0;
+        let now             = Date.now();
+        let canSendNotifcation  = true;
 
         try {
-        URL = /-u,([^,]+),/gm.exec(PARAMETERS)[1];
-        TEXT = /-t,([^,]+)/gm.exec(PARAMETERS)[1];
-        DELAY = /-d,([^,]+)/gm.exec(PARAMETERS)[1];
-        DELAY = parseInt(DELAY);
+            URL = /-u,([^,]+),/gm.exec(PARAMETERS)[1];
+            TEXT = /-t,([^,]+),/gm.exec(PARAMETERS)[1];
+            DELAY = /-d,([^,]+),/gm.exec(PARAMETERS)[1];
+            TIMEOUT = /-w,([^,]+)/gm.exec(PARAMETERS)[1];
+            DELAY = parseInt(DELAY);
         } catch {
-            console.error("Missing url (-u) and/or texte (-t) and/or delay (-d) in secondes");
+            console.error("Missing url (-u) and/or texte (-t) and/or delay (-d) in minutes timeout (-w) in secondes");
             console.log("");
-            console.error("uptimed.js -u [URL] -t [TEXT] -d [DELAY]");
+            console.error("uptimed.js -u [URL] -t [TEXT] -d [DELAY] -w [TIMEOUT]");
             console.log("");
             return;
         }
 
+        console.log(`Search '${TEXT}' in '${URL}' every ${DELAY} minute(s) and wait ${TIMEOUT} secondes max.`);
+
+while (true) {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(URL);
         await page.screenshot({ path: 'example.png' });
-        await browser.close();
 
-        console.log(`Text >${TEXT}< in '${URL}' every ${DELAY} secondes`);
-        await page.waitForTimeout(DELAY * 1000);
+        try {
+            await page.goto(URL, {'timeout': TIMEOUT * 1000, 'waitUntil':'load'});
+            // https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
+            upAndRunning    = true;
+        } catch {
+            upAndRunning    = false;
+        }
+        
+        let textRegex = new RegExp(TEXT,"gi")
+        const textFound = (await page.content()).match(textRegex);
+
+        if (textFound !== null && upAndRunning){
+            upAndRunning    = true;
+        }else{
+            upAndRunning    = false;
+        }
+
+        now = Date.now();
+        minimumBetweenTwoNotification = 1000 * (60 * 60); // ms * secondes * minutes
+        canSendNotifcation = (now - lastNotification) > minimumBetweenTwoNotification; 
+
+        if (canSendNotifcation && !upAndRunning) {
+            lastNotification = Date.now();
+            sendNofication(URL, TEXT, lastNotification);
+        }else {
+            process.stdout.write('.');
+        }
+
+        await browser.close();
+        await page.waitForTimeout( ( DELAY * 60 ) * 1000 );
     }
 
 })();
+
+function sendNofication(URL, TEXT, lastNotification) {
+    
+    let txt = `${lastNotification} = '${TEXT}' not found in '${URL}'`;
+    var data = JSON.stringify({
+        'user': "", 'pass': "", 'msg': txt
+    });
+
+    const options = {
+        hostname: 'smsapi.free-mobile.fr',
+        port: 443,
+        path: '/sendmsg',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        //   'Content-Length': data.length
+        }
+    }
+
+    const req = https.request(options, res => {
+        // console.log(`statusCode: ${res.statusCode}`)
+        res.on('data', d => {
+          console.log(`>${d}<`);
+        })
+    })
+
+    req.on('error', error => {
+        console.error(error);
+    })
+
+    req.write(data);
+    req.end();
+
+    console.log(`Sent >>> ${txt}`);
+}
